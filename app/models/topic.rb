997cf058cb8c753423deaa11680682c26fbeaf90
Atte Tamminen <atukka@gmail.com>
2022-12-29 11:50:38 +0200

    search on topic texts

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ApplicationRecord
   belongs_to :author
   has_many :posts
+
+  include PgSearch::Model
+  multisearchable against: :topic
 end
--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class TopicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "finds In a Wicked Age topics" do
+    TopicPersister.crawl_file(file_fixture("iawa_topic_1_no_brackets_5814.html"))
+    TopicPersister.crawl_file(file_fixture("iawa_topic_2_brackets_6392.html"))
+    iawa_documents = PgSearch.multisearch("iawa")
+
+    assert_not_empty iawa_documents
+    assert_equal 2, iawa_documents.size
+  end
 end
